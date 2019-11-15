584b62e139a1507d0cd9af6ac7fdf65f8f721567
divierc <divierc@msn.com>
2019-11-15 08:05:40 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+import { ToastController } from '@ionic/angular';
+import { element } from 'protractor';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  qrData = 'https://ionicacademy.com/';
+  scannedCode = null;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
 
-  constructor() {}
+
+  constructor(private barcodeScanner: BarcodeScanner, private base64ToGallery: Base64ToGallery, private toastCtrl: ToastController) {}
+
+  scanCode() {
+    this.barcodeScanner.scan().then(
+      barcodeData => {
+        this.scannedCode = barcodeData;
+      }
+    )
+  }
+
+  downloadQR() {
+    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+    const imageData = canvas.toDataURL('image/jpeg').toString();
+    console.log('data: ', imageData);
+
+    let data = imageData.split(',')[1];
+    console.log('dataSplit: ', data);
+
+    this.base64ToGallery.base64ToGallery(data, { prefix: '_img', mediaScanner: true})
+    .then(async res => {
+        let toast = await this.toastCtrl.create({
+            header: 'QR Code saved in your Photolibrary'
+        });
+    }, err => console.log('err: ', err)
+    );
+  }
 
 }
